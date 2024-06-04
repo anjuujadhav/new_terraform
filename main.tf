deb37d2954b59ad19f83bb09c794b3e105cfd022
anjuujadhav <166729316+anjuujadhav@users.noreply.github.com>
2024-06-04 13:12:52 +0530

    ec2 new

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ module "vpc" {
 
 module "ec2" {
     source = "./module/ec2"
-    this_aws_instance_ami = "ami-0eb5115914ccc4bc2"
+    this_aws_instance_ami = "ami-00beae93a2d981137"
     this_aws_instance_instance_type  = "t2.micro"
     //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
-    this_aws_instance_key_name = "delete_oregon_anup"
-    this_aws_instance_availability_zone = "us-west-2b"
+    this_aws_instance_key_name = "minicube"
+    this_aws_instance_availability_zone = "us-east-1a"
     this_aws_instance_volume_size = 15
     this_aws_instance_count = 2
     this_aws_instance_user_data_base64 = true
